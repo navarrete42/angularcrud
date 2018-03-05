dc10b19fc2a2e6937cbc97d49ecff7824b462986
Diego Navarrete <navarrete42@gmail.com>
2018-03-04 20:52:25 -0600

    se agrega la tabla de datos

--- src/app/employees/employee-list/employee-list.component.ts
+++ src/app/employees/employee-list/employee-list.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../shared/employee.service'
 import { Employee } from '../shared/employee.model';
 
+
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
@@ -13,6 +14,7 @@ export class EmployeeListComponent implements OnInit {
   constructor(private employeeService : EmployeeService) { }
 
   ngOnInit() {
+    this.employeeService.getEmployeeList();
   }
 
 }
--- src/app/employees/shared/employee.service.ts
+++ src/app/employees/shared/employee.service.ts
@@ -11,7 +11,7 @@ export class EmployeeService
 {
 
  selectedEmployee: Employee;
- 
+ employeeList: Employee[];
   constructor(private http : Http) {}
 
   postEmployee(emp : Employee)
@@ -20,6 +20,16 @@ export class EmployeeService
      var headerOptions= new Headers({'Content-Type':'application/json'});
      var requestOptions= new RequestOptions({method : RequestMethod.Post, headers : headerOptions});
      return this.http.post('https://jsonplaceholder.typicode.com/posts',body,requestOptions).map(x => x.json());
+  }
+
+  getEmployeeList()
+  {
+    this.http.get('https://jsonplaceholder.typicode.com/posts')
+    .map((data : Response ) => {
+      return data.json() as Employee[];
+    }).toPromise().then(x => {
+      this.employeeList = x;
+    });
   }
   
 
